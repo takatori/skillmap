81dda561647ae7b407ef3a27902ab648e310e718
鷹取敏志 <takatorix@gmail.com>
2020-08-25 23:30:03 +0900

    refactor

--- api/src/main/scala/skills/presentation/route/UserRoute.scala
+++ api/src/main/scala/skills/presentation/route/UserRoute.scala
@@ -1,48 +1,77 @@
 package skills.presentation.route
 
 import org.http4s.HttpRoutes
-import skills.presentation.response.{InternalServerErrorResponse, NotFoundResponse}
+import skills.presentation.response.{ErrorResponse, InternalServerErrorResponse, NotFoundResponse}
 import skills.presentation.route.Route.Service
 import skills.usecase.UserUseCase
 import sttp.model.StatusCode
 import sttp.tapir.json.circe.jsonBody
-import sttp.tapir.ztapir.{endpoint, oneOf, path, statusMapping, stringBody}
+import sttp.tapir.ztapir.{endpoint, oneOf, path, statusMapping}
 import zio.{Task, ZIO, ZLayer}
 import sttp.tapir.server.http4s.ztapir._
 import sttp.tapir.ztapir._
 import io.circe.generic.auto._
+import skills.domain.failure.ExpectedFailure
 import skills.domain.user.UserId
 import zio.interop.catz._
 
+case class UserResponse(id: String, name: String)
+case class UserForm(name: String)
+
 object UserRoute {
 
-  val live: ZLayer[UserUseCase, Nothing, Route] =
-    ZLayer.fromService { usecase =>
-      val endpointWithLogic: HttpRoutes[Task] =
-        getUserEndPoint.toRoutes(id =>
-          usecase.get(new UserId(id)).map(_.toString).orElse(ZIO.fail(NotFoundResponse("")))
-        )
+  import Endpoints._
+  import Logics._
 
+  val live: ZLayer[UserUseCase, Nothing, Route] =
+    ZLayer.fromService { implicit usecase =>
+      val routes: HttpRoutes[Task] = getUserEndPoint.toRoutes(getUserLogic(_))
       new Service {
         override def route: ZIO[Any, Any, HttpRoutes[Task]] =
-          ZIO.succeed(endpointWithLogic)
+          ZIO.succeed(routes)
       }
     }
 
-  private val getUserEndPoint = endpoint.get
-    .in("user" / path[String]("user id"))
-    .errorOut(
-      oneOf(
-        statusMapping(
-          StatusCode.InternalServerError,
-          jsonBody[InternalServerErrorResponse]
-        ),
-        statusMapping(
-          StatusCode.NotFound,
-          jsonBody[NotFoundResponse]
+  object Logics {
+
+    def getUserLogic(id: String)(implicit userUseCase: UserUseCase.Service): ZIO[Any, ErrorResponse, UserResponse] =
+      userUseCase
+        .get(new UserId(id))
+        .map(u => UserResponse(u.id.value, u.name))
+        .catchAll {
+          case _: ExpectedFailure => ZIO.fail(NotFoundResponse(s"user not found $id."))
+          case e                  => ZIO.fail(InternalServerErrorResponse(s"$e", "", ""))
+        }
+
+  }
+
+  object Endpoints {
+    val userEndpoint = endpoint
+      .errorOut(
+        oneOf(
+          statusMapping(
+            StatusCode.InternalServerError,
+            jsonBody[InternalServerErrorResponse]
+          ),
+          statusMapping(
+            StatusCode.NotFound,
+            jsonBody[NotFoundResponse]
+          )
         )
       )
-    )
-    .out(stringBody)
+      .in("user")
+
+    val getUserEndPoint = userEndpoint.get
+      .in(path[String]("user id"))
+      .out(jsonBody[UserResponse])
+
+    val registerUser = userEndpoint.post
+      .in(
+        jsonBody[UserForm]
+          .description("Register User")
+          .example(UserForm("Test User Name"))
+      )
+
+  }
 
 }
