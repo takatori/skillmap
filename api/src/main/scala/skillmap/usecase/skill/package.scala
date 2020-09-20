5d23de7a6c2afd2d0582f260e47787f672ed55ff
鷹取敏志 <takatorix@gmail.com>
2020-09-20 23:34:28 +0900

    refactor

--- api/src/main/scala/skillmap/usecase/skill/package.scala
+++ api/src/main/scala/skillmap/usecase/skill/package.scala
@@ -1,8 +1,18 @@
 package skillmap.usecase
 
-import zio.Has
+import skillmap.domain.failure.ExpectedFailure
+import skillmap.domain.skill.{Skill, SkillId}
+import zio.{Has, ZIO}
 
 package object skill {
   type SkillUseCase = Has[SkillUseCase.Service]
 
+  def get(id: SkillId): ZIO[SkillUseCase, ExpectedFailure, Skill] =
+    ZIO.accessM(_.get.get(id))
+
+  def register(name: String, description: Option[String]): ZIO[SkillUseCase, ExpectedFailure, Unit] =
+    ZIO.accessM(_.get.register(name, description))
+
+  def remove(id: SkillId): ZIO[SkillUseCase, ExpectedFailure, Unit] =
+    ZIO.accessM(_.get.remove(id))
 }
