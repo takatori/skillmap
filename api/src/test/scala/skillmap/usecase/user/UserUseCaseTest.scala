7a42166f229f442b836af23b21151826474e8bae
鷹取敏志 <takatorix@gmail.com>
2020-10-08 23:57:20 +0900

    add test

--- api/src/test/scala/skillmap/usecase/user/UserUseCaseTest.scala
+++ api/src/test/scala/skillmap/usecase/user/UserUseCaseTest.scala
@@ -1,16 +1,19 @@
 package skillmap.usecase.user
 
-import skillmap.domain.user.UserId
-import zio.ZIO
+import skillmap.domain.user.{LiveUserRepository, User, UserId}
+import skillmap.infrastructure.id.IdFactory
+import skillmap.usecase.user
+import zio.blocking.Blocking
 import zio.test.Assertion._
 import zio.test._
+import zio.{ZIO, ZLayer}
 
 object UserUseCaseTest extends DefaultRunnableSpec {
 
-  // private val layer: ZLayer[Any, Throwable, UserUseCase] =
-  // (Blocking.live >>> LiveUserRepository.live ++ IdFactory.live) >>> UserUseCase.live
+  private val layer: ZLayer[Any, Throwable, UserUseCase] =
+    (Blocking.live >>> LiveUserRepository.live ++ IdFactory.live) >>> UserUseCase.live
 
-  val test =
+  val test: ZSpec[Any, Nothing] =
     testM("returns none") {
       val id = UserId("abc")
       //val result = user.get(id)
@@ -18,8 +21,17 @@ object UserUseCaseTest extends DefaultRunnableSpec {
       ZIO.succeed(assert(id)(equalTo(id)))
     }
 
+  val test2 =
+    testM("") {
+      val id = UserId("abc")
+      for {
+        result <- user.get(id).provideLayer(layer)
+      } yield assert(result)(equalTo(User(id, "")))
+    }
+
   def spec =
     suite("UserUseCase")(
-      test
+      test,
+      test2
     )
 }
