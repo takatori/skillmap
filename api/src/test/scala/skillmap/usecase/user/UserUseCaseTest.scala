fe6e2b028f92ac916dd9889445ca6101829fa8f2
鷹取敏志 <takatorix@gmail.com>
2020-10-09 23:47:25 +0900

    fix unit test

--- api/src/test/scala/skillmap/usecase/user/UserUseCaseTest.scala
+++ api/src/test/scala/skillmap/usecase/user/UserUseCaseTest.scala
@@ -1,37 +1,29 @@
 package skillmap.usecase.user
 
-import skillmap.domain.user.{LiveUserRepository, User, UserId}
+import skillmap.domain.failure.NotFoundFailure
+import skillmap.domain.user.{LiveUserRepository, UserId}
 import skillmap.infrastructure.id.IdFactory
 import skillmap.usecase.user
+import zio.ZLayer
 import zio.blocking.Blocking
 import zio.test.Assertion._
 import zio.test._
-import zio.{ZIO, ZLayer}
 
 object UserUseCaseTest extends DefaultRunnableSpec {
 
   private val layer: ZLayer[Any, Throwable, UserUseCase] =
     (Blocking.live >>> LiveUserRepository.live ++ IdFactory.live) >>> UserUseCase.live
 
-  val test: ZSpec[Any, Nothing] =
-    testM("returns none") {
-      val id = UserId("abc")
-      //val result = user.get(id)
-      //assertM(result)(equalTo(User(id, "")))
-      ZIO.succeed(assert(id)(equalTo(id)))
-    }
-
-  val test2 =
-    testM("") {
+  val testNotFoundFailure =
+    testM("returns NotFoundFailure") {
       val id = UserId("abc")
       for {
-        result <- user.get(id).provideLayer(layer)
-      } yield assert(result)(equalTo(User(id, "")))
+        result <- user.get(id).provideLayer(layer).run
+      } yield assert(result)(fails(equalTo(NotFoundFailure(s"user($id) not found."))))
     }
 
   def spec =
     suite("UserUseCase")(
-      test,
-      test2
+      testNotFoundFailure
     )
 }
