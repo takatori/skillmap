3944ad4afcf5a7238579720041f758f6fdd835f4
鷹取敏志 <takatorix@gmail.com>
2020-10-28 23:58:12 +0900

    add test

--- api/src/test/scala/skillmap/usecase/skill/SkillUseCaseSpec.scala
+++ api/src/test/scala/skillmap/usecase/skill/SkillUseCaseSpec.scala
@@ -1,7 +1,11 @@
 package skillmap.usecase.skill
 
-import skillmap.domain.skill.SkillRepository
+import skillmap.domain.skill.{Skill, SkillId, SkillRepository}
+import skillmap.infrastructure.id.IdFactory
+import skillmap.usecase.skill
+import zio.test.Assertion._
 import zio.test._
+import zio.test.mock.Expectation._
 import zio.test.mock.mockable
 
 @mockable[SkillRepository.Service]
@@ -11,6 +15,14 @@ object SkillUseCaseSpec extends DefaultRunnableSpec {
 
   def spec =
     suite("`SkillUseCase.get` must")(
-      testM("") {}
+      testM("return `Skill`") {
+        val id             = SkillId("test")
+        val testSkill      = Skill(id, "test skill", None)
+        val mockRepository = MockSkillRepository.Get(equalTo(id), value(Some(testSkill)))
+        val layer          = (IdFactory.test ++ mockRepository) >>> SkillUseCase.live
+        for {
+          result <- skill.get(id).provideLayer(layer)
+        } yield assert(result)(equalTo(testSkill))
+      }
     )
 }
