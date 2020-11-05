f92512196817734059bcfed5a09a3286d9b59474
鷹取敏志 <takatorix@gmail.com>
2020-11-05 23:58:02 +0900

    apply Refined to UserId

--- api/src/main/scala/skillmap/domain/user/UserId.scala
+++ api/src/main/scala/skillmap/domain/user/UserId.scala
@@ -1,3 +1,17 @@
 package skillmap.domain.user
 
-case class UserId(value: String) extends AnyVal
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.boolean.And
+import eu.timepit.refined.char.{LetterOrDigit, LowerCase}
+import eu.timepit.refined.refineV
+import io.estatico.newtype.macros.newtype
+import skillmap.domain.user.UserId.UserIdString
+
+@newtype case class UserId(value: UserIdString)
+object UserId {
+  type UserIdRule   = LetterOrDigit And LowerCase
+  type UserIdString = String Refined UserIdRule
+
+  def apply(rawUserId: String): Either[String, UserId] =
+    refineV[UserIdRule](rawUserId).map(UserId(_))
+}
