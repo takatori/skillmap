e7f94a22778b856ac23146ac3cad8235bc1e8519
鷹取敏志 <takatorix@gmail.com>
2020-11-06 23:53:21 +0900

    refactor

--- api/src/main/scala/skillmap/domain/user/User.scala
+++ api/src/main/scala/skillmap/domain/user/User.scala
@@ -1,3 +1,21 @@
 package skillmap.domain.user
 
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.refineV
+import eu.timepit.refined.string.Uuid
+import io.estatico.newtype.macros.newtype
+import skillmap.domain.user.User.UserId
+
 final case class User(id: UserId, name: String)
+
+object User {
+
+  type UserIdRule   = Uuid
+  type UserIdString = String Refined UserIdRule
+  @newtype case class UserId(value: UserIdString)
+  object UserId {
+    def apply(rawUserId: String): Either[String, UserId] =
+      refineV[UserIdRule](rawUserId).map(UserId(_))
+  }
+
+}
